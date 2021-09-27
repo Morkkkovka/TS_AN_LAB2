640e13de02ad3bf2a880b9b368a83e31b556ac5f
Morkkkovka <aaa988248@gmail.com>
2021-09-27 20:03:54 +0300

    four task

---
+++ TS AN LAB2 (task 4)/TS AN LAB2 (task 4)/Program.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TS_AN_LAB2__task_4_
+{
+    class Point
+    {
+        string name;
+        int x, y;
+        public int X
+        {
+            get
+            {
+                return x;
+            }
+        }
+        public int Y
+        {
+            get
+            {
+                return y;
+            }
+        }
+        public string Name
+        {
+            get
+            {
+                return name;
+                
+            }
+            
+        }
+        
+        public Point()
+            : this("", 0, 0) { }
+        public Point(string name, int x, int y)
+        {
+            Console.WriteLine("Введіть букву сторони:");
+            this.name = Console.ReadLine();
+            Console.WriteLine("Введіть X:");
+            this.x = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введіть Y:");
+            this.y = int.Parse(Console.ReadLine());
+        }
+    }
+
+    class Figure
+    {
+        int number;
+        Point[] points;
+        string name;
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+            }
+        }
+        public void InitializeFigure()
+        {
+            Console.WriteLine("Введіть кількість сторін фігури:");
+            number = int.Parse(Console.ReadLine());
+            points = new Point[number];
+            for (int i = 0; i < points.Length; i++)
+            {
+                points[i] = new Point();
+            }
+
+        }
+        public double LengthSide(Point a, Point b)
+        {
+            return Math.Sqrt(Math.Pow(a.X - b.X, 2) + Math.Pow(a.Y - b.Y, 2));
+        }
+        public double CalculatePerimeter()
+        {
+            double perimeter = 0;
+            for (int i = 1; i < points.Length; i++)
+            {
+                perimeter += this.LengthSide(points[i - 1], points[i]);
+            }
+            perimeter += this.LengthSide(points[0], points[points.Length - 1]);
+            return perimeter;
+        }
+        public Figure(string name)
+        {
+            InitializeFigure();
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            Figure cube = new Figure("cube");
+            Console.Write("Периметр фігури!\n");
+              
+            Console.WriteLine(cube.CalculatePerimeter());
+            
+
+            Console.ReadKey();
+        }
+    }
+}
