f3d37ad6adf3dcf94d6dcecfd496746378fc5dd9
Morkkkovka <aaa988248@gmail.com>
2021-09-27 20:04:45 +0300

    fifth

---
+++ TS AN LAB2 (task 5)/TS AN LAB2 (task 5)/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TS_AN_LAB2__task_5_
+{
+    class User
+    {
+        public string login;
+        public string name;
+        public string surname;
+        public int age;
+        private DateTime date = DateTime.Today;
+
+    }
+
+    public void GetInfo()
+    {
+        Console.WriteLine($"Логін: {login} \nІм'я: {name} \nПрізвище: {surname} \nВік: {age} \nДата реєстрації: {date.ToString("D")}\n");
+    }
+
+    class Program
+
+    {
+
+        static void Main(string[] args)
+
+        {
+
+            User user = new User();
+
+            user.login = "A.Tsipur.121.20@knute.edu.ua";
+
+            user.name = "Анна";
+
+            user.surname = "Ціпур";
+
+            user.age = "18";
+
+            user.GetInfo();
+
+            Console.ReadKey();
+
+        }
+    }
+
+}
